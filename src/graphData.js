b0f912929896ab862ff3a40af314b25b769448bc
Jan Varsava <janvalentine@gmail.com>
2021-03-20 12:42:30 -0400

    node labels

--- src/graphData.js
+++ src/graphData.js
@@ -10,7 +10,7 @@ let nodes = [
   },
   {
     id: 1,
-    label: "BNYN2018",
+    label: "Bakhti, 2018",
     authors:
       "Khadidja Bakhti, Zhendong Niu, Abdallah Yousif, and Ally S Nyamawe",
     title:
@@ -21,76 +21,76 @@ let nodes = [
   },
   {
     id: 2,
-    label: "BW2006",
+    label: "Bergstrom, 2006",
     authors: "Peter  Bergstrom  and  E  James  Whitehead  Jr.",
     title:
       "CircleView:  Scalable visualization and navigation of citation networks",
     link:
       "https://www.semanticscholar.org/paper/CircleView-%3A-Scalable-Visualization-and-Navigation-Bergstr%C3%B6m-Whitehead/a7ce5d57da34fac3ef776be1d0fbaa70d92b68af",
-    x: 48,
-    y: -11,
+    x: 41,
+    y: -10,
   },
   {
     id: 3,
-    label: "DS2011",
+    label: "Dong, 2011",
     authors: "Cailing Dong and Ulrich Schafer",
     title: "Ensemble-style self-training on citation classification",
     link: "https://www.aclweb.org/anthology/I11-1070/",
     x: 42,
-    y: 26,
+    y: 23,
   },
   {
     id: 4,
-    label: "E2009",
+    label: "Eisner, 2009",
     authors: "Jason Eisner",
     title: "How to read a technical paper",
     link: "http://cs.jhu.edu/~jason/advice/how-to-read-a-paper.html",
-    x: -19,
+    x: -12,
     y: 37,
   },
   {
     id: 5,
-    label: "ET2007",
+    label: "Elmqvist, 2007",
     authors: "Niklas Elmqvist and Philippas Tsigas",
     title:
       "CiteWiz: a tool for the visualization of scientific citation networks",
     link:
       "https://www.researchgate.net/publication/220586688_CiteWiz_A_Tool_for_the_Visualization_of_Scientific_Citation_Networks",
-    x: -42,
-    y: -7,
+    x: -33,
+    y: -8,
   },
   {
     id: 6,
-    label: "K2007",
+    label: "Keshav, 2007",
     authors: "Srinivasan Keshav",
     title: "How to read a paper",
     link: "http://ccr.sigcomm.org/online/files/p83-keshavA.pdf",
-    x: -35,
-    y: -18,
+    x: -28,
+    y: -20,
   },
   {
     id: 7,
-    label: "PH2004",
+    label: "Purugganan, 2004",
     authors: "Mary Purugganan and Jan Hewit",
     title: "How to read a scientific article",
     link:
       "https://www.owlnet.rice.edu/~cainproj/courses/HowToReadSciArticle.pdf",
-    x: -44,
+    x: -32,
     y: 5,
   },
   {
     id: 8,
-    label: "SK2010",
+    label: "Schafer, 2010",
     authors: "Ulrich Schafer and Uwe Kasterka",
     title:
       "Scientific authoring support: A tool to navigate in typed citation graphs",
     link: "https://www.aclweb.org/anthology/W10-0402/",
-    x: -31,
-    y: 28,
+    x: -22,
+    y: 27,
   },
   {
     id: 9,
-    label: "R1977",
+    label: "Rosing, 1977",
     authors: "Ina Spiegel-Rosing",
     title: "Science studies: bibliometric and content analysis",
     link: "https://journals.sagepub.com/doi/10.1177/030631277700700111",
@@ -99,7 +99,7 @@ let nodes = [
   },
   {
     id: 10,
-    label: "TST2006",
+    label: "Teufel, 2006",
     authors: "Simone Teufel, Advaith Siddharthan, and Dan Tidhar",
     title: "An annotation scheme for citation function",
     link: "https://www.cl.cam.ac.uk/~sht25/papers/sigdial06.pdf",
@@ -108,16 +108,16 @@ let nodes = [
   },
   {
     id: 11,
-    label: "WS 2012",
+    label: "Weitz, 2012",
     authors: "Benjamin Weitz and Ulrich Schafer",
     title: "A graphical citation browser for the ACL anthology",
     link: "https://www.aclweb.org/anthology/L12-1474/",
-    x: -40,
-    y: 18,
+    x: -36,
+    y: 17,
   },
   {
     id: 12,
-    label: "XMM 2013",
+    label: "Xu, 2013",
     authors: "Han Xu, Eric Martin, and Ashesh Mahidadia",
     title:
       "Using heterogeneous features for scientific citation classification",
@@ -128,7 +128,7 @@ let nodes = [
   },
   {
     id: 13,
-    label: "HKI2016",
+    label: "Han, 2016",
     authors: "Dongli Han, Hiroshi Koide, and Ayato Inoue",
     title:
       "Acquisition of scientific literatures based on citation-reason visualization",
@@ -138,7 +138,7 @@ let nodes = [
   },
   {
     id: 14,
-    label: "KTX2020",
+    label: "Kohavi, 2020",
     authors: "Ron Kohavi, Diane Tang, and Ya Xu",
     title:
       "Trustworthy online controlled experiments: A practical guide to A/B testing",
@@ -149,16 +149,16 @@ let nodes = [
   },
   {
     id: 15,
-    label: "MM1975",
+    label: "Moravcsik, 1975",
     authors: "Michael J Moravcsik and Poovanalingam Murugesan",
     title: "Some results on the function and quality of citations",
     link: "https://www.jstor.org/stable/284557?seq=1",
-    x: 47,
-    y: 16,
+    x: 43,
+    y: 13,
   },
   {
     id: 16,
-    label: "Z1968",
+    label: "Ziman, 1968",
     authors: "John M Ziman",
     title:
       "Public Knowledge: An Essay Concerning the Social Dimension of Science",
